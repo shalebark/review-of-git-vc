07192b6a939a1ab36f601cb8dc95b6e47d08ffb6
U-kenneth-PC\kenneth <cremling@protonmail.com>
2020-08-25 03:02:25 -0400

    uses the difflib library instead

--- diff.py
+++ diff.py
@@ -1,206 +1,28 @@
-class Differ:
+import sys
+import os
+import difflib
 
-    def __init__(self, original, modified):
-        self.buffers = {'original': original, 'modified': modified}
+def diff(buff1, buff2):
+    lines1 = buff1.split(os.linesep)
+    lines2 = buff2.split(os.linesep)
+    return list(difflib.unified_diff(lines1, lines2, n=0))[2:]
 
-        self.lines = {'original': self.buffers['original'].split('\n'), 'modified': self.buffers['modified'].split('\n')}
-        self.pointer = { 'original': { 'position': -1, 'line': None, 'eol': False }, 'modified': { 'position': -1, 'line': None, 'eol': False } }
+def diff_files(file1, file2):
+    buff1 = None
+    buff2 = None
+    with open(file1) as f1, open(file2) as f2:
+        buff1 = f1.read()
+        buff2 = f2.read()
+    return diff(buff1, buff2)
 
-        self.changes = []
+def diff_buff(deltas):
+    return os.linesep.join(deltas)
 
-    # --- GETTER HELPERS
-    def get_buffer_lines(self, buffer_name):
-        return self.lines[buffer_name]
+def diff_out(deltas):
+    sys.stdout.write(diff_buff(deltas))
 
-    def get_buffer_length(self, buffer_name):
-        return len(self.get_buffer_lines(buffer_name))
-
-    def get_pointer_position(self, buffer_name):
-        return self.pointer[buffer_name]['position']
-
-    def get_pointer_line(self, buffer_name):
-        return self.pointer[buffer_name]['line']
-
-    def get_pointer_eol(self, buffer_name):
-        return self.pointer[buffer_name]['eol']
-    # --- EOF GETTER HELPERS
-
-    def read_next_line(self, buffer_name):
-        if self.get_pointer_eol(buffer_name) == True:
-            return self.pointer
-
-        self.pointer[buffer_name]['position'] += 1
-
-        if self.get_pointer_position(buffer_name) >= self.get_buffer_length(buffer_name):
-            self.pointer[buffer_name]['line'] = None
-            self.pointer[buffer_name]['eol'] = True
-            return self.pointer
-
-        self.pointer[buffer_name]['line'] = None if self.get_pointer_position(buffer_name) < 0 else self.get_buffer_lines(buffer_name)[self.get_pointer_position(buffer_name)]
-        return self.pointer
-
-    def read_line(self):
-        self.read_next_line('original')
-        self.read_next_line('modified')
-        return not (self.get_pointer_eol('original') or self.get_pointer_eol('modified'))
-
-    def line_is_changed(self):
-        return self.get_pointer_line('original') != self.get_pointer_line('modified')
-
-    def seek_pointer_position(self, buffer_name, index):
-        self.pointer[buffer_name]['eol'] = False
-        self.pointer[buffer_name]['position'] = index - 1;
-        self.read_next_line(buffer_name)
-
-    def find_next_line_position(self, buffer_name, lookup_line):
-        starting_position = self.get_pointer_position(buffer_name)
-        for position in range(starting_position, self.get_buffer_length(buffer_name)):
-            if self.get_buffer_lines(buffer_name)[position] == lookup_line:
-                return position
-        return None
-
-    def delete_by_position_range(self, start_position, end_position):
-        self.changes.append({'type': 'sub', 'start_position': start_position, 'length': end_position - start_position})
-
-    def add_by_position_range(self, start_position, end_position):
-        for position in range(start_position, end_position):
-            self.changes.append({'type': 'add', 'position': position, 'line': self.get_buffer_lines('modified')[position] })
-
-    def delete_remaining_original_lines(self):
-        self.delete_by_position_range(self.get_pointer_position('original'), self.get_buffer_length('original'))
-
-    def add_remaining_modified_lines(self):
-        while True:
-            self.add(self.get_pointer_position('modified'), self.get_pointer_line('modified'))
-            self.read_next_line('modified')
-            if self.get_pointer_eol('modified'):
-                break
-
-
-def diff(file1, file2):
-    with open(file1, 'r') as f1, open(file2, 'r') as f2:
-        differ = Differ(f1.read(), f2.read())
-
-        while True:
-            differ.read_next_line('original')
-            differ.read_next_line('modified')
-
-            # end condition: both files has finally been exhausted, exit
-            if differ.get_pointer_eol('original') and differ.get_pointer_eol('modified'):
-                break
-
-            # end condition: the original file is larger than the modified, remaining originals must have been deleted
-            if differ.get_pointer_eol('modified'):
-                differ.delete_by_position_range(differ.get_pointer_position('original'), differ.get_buffer_length('original'))
-                break
-
-            # end condition: the modified file is larger than the original, remaining modified must be new additions
-            if differ.get_pointer_eol('original'):
-                differ.add_by_position_range(differ.get_pointer_position('modified'), differ.get_pointer_position('modified'))
-                break
-
-            if differ.get_pointer_line('original') != differ.get_pointer_line('modified'):
-                found_position = differ.find_next_line_position('original', differ.get_pointer_line('modified'))
-
-                # Nothing is Found, then this is a new line, add
-                if found_position is None:
-                    differ.add_by_position_range(differ.get_pointer_position('modified'), differ.get_pointer_position('modified') + 1)
-                    # Shift Up the original pointer by 1, so we can go back to this same original line for next check
-                    differ.seek_pointer_position('original', differ.get_pointer_position('original') - 1)
-
-                # If a position is found, then we've deleted all the previous lines
-                else:
-                    differ.delete_by_position_range(differ.get_pointer_position('original'), found_position)
-                    # Move the original pointer to the new position, the current modified matches this one, so move the pointer so they both match, next loop will increment both
-                    differ.seek_pointer_position('original', found_position)
-
-
-        # while differ.read_line():
-        #     if differ.line_is_changed():
-        #         print('preindex lookup',
-        #             differ.get_buffer_length('original'), differ.get_buffer_length('modified'),
-        #             differ.get_buffer_lines('original'), differ.get_buffer_lines('modified'),
-        #             differ.get_pointer_position('original'), differ.get_pointer_line('original'),
-        #             differ.get_pointer_position('modified'), differ.get_pointer_line('modified'),
-        #         )
-
-        #         original_index = differ.get_pointer_position('original')
-        #         found_index = differ.lookup_next_instance(differ.get_pointer_line('modified'))
-
-        #         print('postindex lookup',
-        #             differ.get_buffer_length('original'), differ.get_buffer_length('modified'),
-        #             differ.get_buffer_lines('original'), differ.get_buffer_lines('modified'),
-        #             differ.get_pointer_position('original'), differ.get_pointer_line('original'),
-        #             differ.get_pointer_position('modified'), differ.get_pointer_line('modified'),
-        #         )
-
-        #         if found_index is not -1:
-        #             differ.delete_by_position_range(original_index, found_index)
-        #         else:
-        #             differ.add(differ.get_pointer_position('modified'), differ.get_pointer_line('modified'))
-        #             differ.set_pointer_position('original', original_index - 1)
-        #     else:
-        #         print( 'no change',
-        #             differ.get_buffer_length('original'), differ.get_buffer_length('modified'),
-        #             differ.get_buffer_lines('original'), differ.get_buffer_lines('modified'),
-        #             differ.get_pointer_eol('original'), differ.get_pointer_eol('modified'),
-        #             differ.get_pointer_position('original'), differ.get_pointer_line('original'),
-        #             differ.get_pointer_position('modified'), differ.get_pointer_line('modified'),
-        #         )
-        #     print('------------------------')
-
-        # print('here')
-
-        # if differ.get_pointer_eol('modified'):
-        #     differ.delete_remaining_original_lines()
-        # if differ.get_pointer_eol('original'):
-        #     differ.add_remaining_modified_lines()
-
-    return {'changes': differ.changes, 'original_lines': differ.get_buffer_lines('original'), 'modified_lines': differ.get_buffer_lines('modified')}
-
-def diff_buff(file1, file2):
-    diff_results = diff(file1, file2)
-
-    changes = diff_results['changes']
-    original_lines = diff_results['original_lines']
-    modified_lines = diff_results['modified_lines']
-
-    buffer = ''
-
-    def add_set_to_buffer():
-        nonlocal buffer
-        nonlocal current_add_set
-
-        if current_add_set:
-            buffer += '+%s,%s\n' %(current_add_set[0]['position'] + 1, len(current_add_set))
-            for add_change in current_add_set:
-                buffer += '>%s\n' %(add_change['line'])
-            current_add_set.clear()
-
-    current_add_set = []
-    for change in changes:
-        if change['type'] == 'add':
-            if not ( current_add_set and ( current_add_set[len(current_add_set) -1]['position'] + 1 == change['position'] ) ):
-                add_set_to_buffer()
-            current_add_set.append(change)
-        # change['type] == 'sub':
-        else:
-            add_set_to_buffer()
-            buffer += '-%s,%s\n' %(change['start_position'] + 1, change['length'])
-            for idx in range(change['start_position'], change['start_position'] + change['length']):
-                print('idx check', idx, original_lines[idx], original_lines)
-                buffer += '<%s\n' %(original_lines[idx])
-
-    add_set_to_buffer()
-
-    return buffer
-
-def diff_out(file1, file2):
-    print(diff(file1, file2))
-    # print(diff_buff(file1, file2))
+def read_deltas_buffer(buff):
+    return list(filter(lambda x: x != '', buff.split(os.linesep)))
 
 if __name__ == "__main__":
-    import sys
-    # for i in range(0, 1):
-    #     print(i)
-    diff_out(sys.argv[1], sys.argv[2])
+    diff_out(diff_files(sys.argv[1], sys.argv[2]))
--- tests/test_diff.py
+++ tests/test_diff.py
@@ -9,30 +9,63 @@ import diff
 class Bin2DecTestCase(unittest.TestCase):
 
     def test_add_from_empty(self):
-        result = diff.diff('tests/test_add_from_empty1', 'tests/test_add_from_empty2')
-        assert(len(result['changes']) == 4)
-        assert(result['changes'][0]['type'] == 'add' and result['changes'][0]['position'] == 0 and result['changes'][0]['line'] == 'a')
-        assert(result['changes'][1]['type'] == 'add' and result['changes'][1]['position'] == 1 and result['changes'][1]['line'] == 'b')
-        assert(result['changes'][2]['type'] == 'add' and result['changes'][2]['position'] == 2 and result['changes'][2]['line'] == 'c')
-        assert(result['changes'][3]['type'] == 'add' and result['changes'][3]['position'] == 3 and result['changes'][3]['line'] == 'd')
+        deltas = diff.diff_files('tests/test_add_from_empty1', 'tests/test_add_from_empty2')
+        assert(len(deltas) == 5)
+        assert('+1,4' in deltas[0])
+        assert(deltas[1] == '+a')
+        assert(deltas[2] == '+b')
+        assert(deltas[3] == '+c')
+        assert(deltas[4] == '+d')
 
     def test_empty_from_full(self):
-        result = diff.diff('tests/test_empty_from_full1', 'tests/test_empty_from_full2')
-        assert(len(result['changes']) == 1)
-        assert(result['changes'][0]['type'] == 'sub')
-        assert(result['changes'][0]['start_position'] == 0)
-        assert(result['changes'][0]['length'] == 4)
+        deltas = diff.diff_files('tests/test_empty_from_full1', 'tests/test_empty_from_full2')
+        assert(len(deltas) == 5)
+        assert('-1,4' in deltas[0])
+        assert(deltas[1] == '-a')
+        assert(deltas[2] == '-b')
+        assert(deltas[3] == '-c')
+        assert(deltas[4] == '-d')
 
     def test_mixed_changes(self):
-        result = diff.diff('tests/test_mixed_changes_1', 'tests/test_mixed_changes_2')
-        assert(len(result['changes']) == 7)
-        assert(result['changes'][0]['type'] == 'add' and result['changes'][0]['position'] == 1 and result['changes'][0]['line'] == 'added line')
-        assert(result['changes'][1]['type'] == 'add' and result['changes'][1]['position'] == 2 and result['changes'][1]['line'] == 'heres another')
-        assert(result['changes'][2]['type'] == 'sub' and result['changes'][2]['start_position'] == 1 and result['changes'][2]['length'] == 1)
-        assert(result['changes'][3]['type'] == 'add' and result['changes'][3]['position'] == 5 and result['changes'][3]['line'] == 'going to add another')
-        assert(result['changes'][4]['type'] == 'add' and result['changes'][4]['position'] == 7 and result['changes'][4]['line'] == 'f line got deleted')
-        assert(result['changes'][5]['type'] == 'add' and result['changes'][5]['position'] == 8 and result['changes'][5]['line'] == 'add some more')
-        assert(result['changes'][6]['type'] == 'sub' and result['changes'][6]['start_position'] == 5 and result['changes'][6]['length'] == 2)
+        deltas = diff.diff_files('tests/test_mixed_changes_1', 'tests/test_mixed_changes_2')
+        assert(len(deltas) == 11)
+        assert('+2,2' in deltas[0])
+        assert(deltas[1] == '-b')
+        assert(deltas[2] == '+added line')
+        assert(deltas[3] == '+heres another')
+        assert('+6' in deltas[4])
+        assert(deltas[5] == '+going to add another')
+        assert('+8,2' in deltas[6])
+        assert(deltas[7] == '-f')
+        assert(deltas[8] == '-g')
+        assert(deltas[9] == '+f line got deleted')
+        assert(deltas[10] == '+add some more')
+
+    def test_diff_buffer(self):
+        deltas = diff.diff_files('tests/test_mixed_changes_1', 'tests/test_mixed_changes_2')
+        result = diff.diff_buff(deltas)
+        expected_result = None
+        with open('tests/mixed_output_result') as f:
+            expected_result = f.read()
+        assert(result.strip() == expected_result.strip())
+
+    def test_diff_read(self):
+        buffer = None
+        with open('tests/mixed_output_result') as f:
+            buffer = f.read()
+        deltas = diff.read_deltas_buffer(buffer)
+        assert(len(deltas) == 11)
+        assert('+2,2' in deltas[0])
+        assert(deltas[1] == '-b')
+        assert(deltas[2] == '+added line')
+        assert(deltas[3] == '+heres another')
+        assert('+6' in deltas[4])
+        assert(deltas[5] == '+going to add another')
+        assert('+8,2' in deltas[6])
+        assert(deltas[7] == '-f')
+        assert(deltas[8] == '-g')
+        assert(deltas[9] == '+f line got deleted')
+        assert(deltas[10] == '+add some more')
 
 if __name__ == '__main__':
     unittest.main()
